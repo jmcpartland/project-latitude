8c6c7a22410f01c1e0d9ac14ff2de46617a4a608
Joseph McPartland <joemcpartland@gmail.com>
2023-07-03 16:54:09 -0600

    style header

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <content>Content</content>
+    )
+}
+
+export default Content
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <p className="title">project_latitude</p>
+    </header>
+  )
+}
+
+export default Header
